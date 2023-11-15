a13372bfa4b69460aeff00ce73855bfa1c00d023
NatChe <chernova.natalia@yahoo.fr>
2023-11-15 09:55:44 +0100

    feat(pipeline): set up oversampling with SMOTENC

--- pipelines.py
+++ pipelines.py
@@ -1,8 +1,11 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.dummy import DummyClassifier
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.pipeline import Pipeline
 from sklearn import set_config
+from imblearn.pipeline import Pipeline as Pipeline_imb
+from imblearn.over_sampling import SMOTENC, SMOTE
 from transformers import ApplicationCleaner, ApplicationImputer, ApplicationEncoder, ApplicationFeaturesExtractor, ApplicationScaler
 
 set_config(transform_output="pandas")
@@ -13,12 +16,14 @@ DEFAULT_CONFIG = {
         'num_imputer': SimpleImputer(strategy='median'),
         'cat_imputer': SimpleImputer(strategy='most_frequent'),
         'should_scale': False,
-        'scaler': StandardScaler()
-    }
+        'scaler': StandardScaler(),
+        'should_oversample': False,
+    },
+    'model_params': {}
 }
 
 
-def get_preprocessing_pipeline(preprocessing_config):
+def get_preprocessing_steps(preprocessing_config):
     steps = [
         ('cleaner', ApplicationCleaner()),
         ('feature_extractor', ApplicationFeaturesExtractor()),
@@ -30,25 +35,50 @@ def get_preprocessing_pipeline(preprocessing_config):
                                          cat_imputer=preprocessing_config['cat_imputer']))
                      )
 
-    steps.append(('encoder', ApplicationEncoder()))
-
     if preprocessing_config['should_scale']:
         steps.append(('scalar', ApplicationScaler(scaler=preprocessing_config['scaler'])))
 
-    return Pipeline(steps=steps)
+    if preprocessing_config['should_oversample']:
+        steps.append(('oversample',
+                      SMOTENC(
+                          categorical_features='auto',
+                          categorical_encoder=OneHotEncoder(sparse_output=False, handle_unknown="ignore"),
+                          random_state=18))
+                     )
+
+    steps.append(('encoder', ApplicationEncoder()))
+
+    return steps
+
+
+def get_preprocessing_pipeline(preprocessing_config):
+    steps = get_preprocessing_steps(preprocessing_config)
+
+    preprocessing_pipeline = Pipeline(steps=steps, verbose=True)
+
+    return preprocessing_pipeline
+
+
+def dummy_classifier_pipeline(config):
+    return DummyClassifier(strategy=config['strategy'])
 
 
 def log_reg_pipeline(config):
-    preprocessing_pipeline = get_preprocessing_pipeline(config['preprocessing'])
+    preprocessing_config = config['preprocessing']
 
-    steps = [
-        ('preprocessor', preprocessing_pipeline),
-        ('classifier', LogisticRegression())
-    ]
+    steps = get_preprocessing_steps(preprocessing_config)
 
-    return Pipeline(steps)
+    if preprocessing_config['should_oversample']:
+        steps.append(('classifier', LogisticRegression(**config['model_params'])))
+        return Pipeline_imb(steps, verbose=True)
+
+    preprocessing_pipeline = Pipeline(steps=steps, verbose=True)
+    steps = [('preprocessor', preprocessing_pipeline), ('classifier', LogisticRegression(**config['model_params']))]
+
+    return Pipeline(steps, verbose=True)
 
 
 PIPELINES = {
-    'log_regression': log_reg_pipeline
+    'log_regression': log_reg_pipeline,
+    'dummy': dummy_classifier_pipeline
 }
--- transformers.py
+++ transformers.py
@@ -7,19 +7,26 @@ from sklearn.preprocessing import OneHotEncoder
 class ApplicationCleaner(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
+        print('Fitting cleaner')
         return self
 
     def transform(self, X):
-        print('Cleaning data...')
+        print('Transforming cleaner')
         X['CODE_GENDER'] = X['CODE_GENDER'].replace('XNA', 'Unknown')
         X['DAYS_EMPLOYED'] = X['DAYS_EMPLOYED'].replace(365243, np.nan)
         X['DAYS_LAST_PHONE_CHANGE'] = X['DAYS_LAST_PHONE_CHANGE'].replace(0, np.nan)
         X['ORGANIZATION_TYPE'] = X['ORGANIZATION_TYPE'].replace('XNA', 'Unknown')
 
         # Map category binary columns to 0 and 1
-        flag_cols_dict = {'N': 0, 'Y': 1}
+        flag_cols_dict = {'N': 0, 'Y': 1, 'No': 0, 'Yes': 1}
         X['FLAG_OWN_CAR'] = X['FLAG_OWN_CAR'].apply(lambda x: flag_cols_dict.get(x))
         X['FLAG_OWN_REALTY'] = X['FLAG_OWN_REALTY'].apply(lambda x: flag_cols_dict.get(x))
+        X['EMERGENCYSTATE_MODE'] = X['EMERGENCYSTATE_MODE'].apply(lambda x: flag_cols_dict.get(x))
+
+        # convert object types to categorical
+        object_columns = X.select_dtypes(['object']).columns
+        for object_column in object_columns:
+            X[object_column] = X[object_column].astype("category")
 
         return X
 
@@ -31,6 +38,7 @@ class ApplicationImputer(BaseEstimator, TransformerMixin):
         self.imputer = None
 
     def fit(self, X, y=None):
+        print('Fitting Imputer')
         numerical_features = X.drop('SK_ID_CURR', axis=1).select_dtypes(['int64', 'float64']).columns
         categorical_features = X.select_dtypes(['object']).columns
 
@@ -46,12 +54,9 @@ class ApplicationImputer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        print('Removing null values...')
-        X_null = X.isnull().sum().sum()
+        print('Transforming Imputer')
 
         X = self.imputer.transform(X)
-        X_null_cleaned = X.isnull().sum().sum()
-        print(f'Removed {X_null - X_null_cleaned} null values')
 
         return X
 
@@ -61,7 +66,8 @@ class ApplicationEncoder(BaseEstimator, TransformerMixin):
         self.encoder = None
 
     def fit(self, X, y=None):
-        categorical_features = X.select_dtypes(['object']).columns
+        print('Fitting Encoder')
+        categorical_features = X.select_dtypes(['category']).columns
 
         transformers = [
             ('cat_encoder', OneHotEncoder(sparse_output=False, handle_unknown="ignore"), categorical_features)
@@ -74,11 +80,9 @@ class ApplicationEncoder(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        print('Encoding values...')
+        print('Transforming Encoder')
 
         X = self.encoder.transform(X)
-
-        print(f'New X shape: {X.shape}')
 
         return X
 
@@ -89,6 +93,7 @@ class ApplicationScaler(BaseEstimator, TransformerMixin):
         self.transformer = None
 
     def fit(self, X, y=None):
+        print('Fitting scalar')
         numerical_features = X.drop('SK_ID_CURR', axis=1).select_dtypes(['int64', 'float64']).columns
         non_binary_numerical_features = [col for col in numerical_features if not col.startswith('FLAG')]
         transformers = [('scaler', self.scaler, non_binary_numerical_features)]
@@ -100,7 +105,7 @@ class ApplicationScaler(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X):
-        print('Scaling values...')
+        print('Transforming scalar')
 
         X = self.transformer.transform(X)
 
@@ -109,10 +114,11 @@ class ApplicationScaler(BaseEstimator, TransformerMixin):
 
 class ApplicationFeaturesExtractor(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
+        print('Fitting FE')
         return self
 
     def transform(self, X):
-        print('Extracting features..')
+        print('Transforming FE')
 
         X['DAYS_EMPLOYED_PERC'] = X['DAYS_EMPLOYED'] / X['DAYS_BIRTH']
         X['INCOME_CREDIT_PERC'] = X['AMT_INCOME_TOTAL'] / X['AMT_CREDIT']
@@ -122,5 +128,4 @@ class ApplicationFeaturesExtractor(BaseEstimator, TransformerMixin):
         X['PAYMENT_RATE'] = X['AMT_ANNUITY'] / X['AMT_CREDIT']
         X['CHILDREN_RATIO'] = X['CNT_CHILDREN'] / X['CNT_FAM_MEMBERS']
 
-        print(f'New X shape: {X.shape}')
         return X
