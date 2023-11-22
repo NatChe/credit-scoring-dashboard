b4a2abca3cb725033b316a4ff4223df72a7231e1
NatChe <chernova.natalia@yahoo.fr>
2023-11-22 11:15:44 +0100

    feat(pipeline): add other datasets, test training

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -14,12 +14,9 @@ APPLICATION_TRAIN_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'applica
 BUREAU_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'bureau.csv')
 BUREAU_BALANCE_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'bureau_balance.csv')
 PREVIOUS_APPLICATION_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'previous_application.csv')
-
-'''credit_card_balance_filepath: YOUR / PATH / TO / credit_card_balance.csv
-installments_payments_filepath: YOUR / PATH / TO / installments_payments.csv
-POS_CASH_balance_filepath: YOUR / PATH / TO / POS_CASH_balance.csv
-sample_submission_filepath: YOUR / PATH / TO / sample_submission.csv
-'''
+POS_CASH_BALANCE_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'POS_CASH_balance.csv')
+INSTALLMENTS_PAYMENTS_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'installments_payments.csv')
+CREDIT_CARD_BALANCE_FILEPATH = os.path.join(ROOT_DIR, DATA_SOURCE_FOLDER, 'credit_card_balance.csv')
 
 
 def load_data(dev_mode=False):
@@ -83,6 +80,8 @@ def get_bureau_and_balance_features(dev_mode=True, nan_as_category=True):
     bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in bb_agg.columns.tolist()])
     bureau = bureau.join(bb_agg, how='left', on='SK_ID_BUREAU')
     bureau.drop(['SK_ID_BUREAU'], axis=1, inplace=True)
+
+    # clean
     del bb, bb_agg
     gc.collect()
 
@@ -124,7 +123,9 @@ def get_bureau_and_balance_features(dev_mode=True, nan_as_category=True):
     closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
     closed_agg.columns = pd.Index(['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
     bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
-    del closed, closed_agg, bureau
+
+    # clean
+    del df_bureau, df_bureau_balance, closed, closed_agg, bureau
     gc.collect()
 
     return bureau_agg
@@ -179,7 +180,102 @@ def get_previous_applications_features(dev_mode=True):
     refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
     refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
     prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
-    del refused, refused_agg, approved, approved_agg, prev
+
+    # clean
+    del df_prev_app, refused, refused_agg, approved, approved_agg, prev
     gc.collect()
 
     return prev_agg
+
+
+def get_pos_cash_balance_features(dev_mode=False):
+    num_rows = DEV_SAMPLE_SIZE if dev_mode else None
+
+    df_pos_cash = pd.read_csv(POS_CASH_BALANCE_FILEPATH, nrows=num_rows)
+    pos_cash, cat_cols = one_hot_encoder(df_pos_cash, nan_as_category=True)
+
+    # Features
+    aggregations = {
+        'MONTHS_BALANCE': ['max', 'mean', 'size'],
+        'SK_DPD': ['max', 'mean'],
+        'SK_DPD_DEF': ['max', 'mean']
+    }
+    for cat in cat_cols:
+        aggregations[cat] = ['mean']
+
+    pos_cash_agg = pos_cash.groupby('SK_ID_CURR').agg(aggregations)
+    pos_cash_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in pos_cash_agg.columns.tolist()])
+
+    # Count pos cash accounts
+    pos_cash_agg['POS_COUNT'] = pos_cash.groupby('SK_ID_CURR').size()
+
+    # clean
+    del pos_cash, df_pos_cash
+    gc.collect()
+
+    return pos_cash_agg
+
+
+def get_installments_payments_features(dev_mode=False):
+    num_rows = DEV_SAMPLE_SIZE if dev_mode else None
+
+    df_installments = pd.read_csv(INSTALLMENTS_PAYMENTS_FILEPATH, nrows=num_rows)
+
+    installments, cat_cols = one_hot_encoder(df_installments, nan_as_category= True)
+
+    # Percentage and difference paid in each installment (amount paid and installment value)
+    installments['PAYMENT_PERC'] = installments['AMT_PAYMENT'] / installments['AMT_INSTALMENT']
+    installments['PAYMENT_DIFF'] = installments['AMT_INSTALMENT'] - installments['AMT_PAYMENT']
+
+    # Days past due and days before due (no negative values)
+    installments['DPD'] = installments['DAYS_ENTRY_PAYMENT'] - installments['DAYS_INSTALMENT']
+    installments['DBD'] = installments['DAYS_INSTALMENT'] - installments['DAYS_ENTRY_PAYMENT']
+    installments['DPD'] = installments['DPD'].apply(lambda x: x if x > 0 else 0)
+    installments['DBD'] = installments['DBD'].apply(lambda x: x if x > 0 else 0)
+
+    # Features: Perform aggregations
+    aggregations = {
+        'NUM_INSTALMENT_VERSION': ['nunique'],
+        'DPD': ['max', 'mean', 'sum'],
+        'DBD': ['max', 'mean', 'sum'],
+       # 'PAYMENT_PERC': ['max', 'mean', 'sum', 'var'],
+        'PAYMENT_DIFF': ['max', 'mean', 'sum', 'var'],
+        'AMT_INSTALMENT': ['max', 'mean', 'sum'],
+        'AMT_PAYMENT': ['min', 'max', 'mean', 'sum'],
+        'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum']
+    }
+    for cat in cat_cols:
+        aggregations[cat] = ['mean']
+
+    installments_agg = installments.groupby('SK_ID_CURR').agg(aggregations)
+    installments_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in installments_agg.columns.tolist()])
+
+    # Count installments accounts
+    installments_agg['INSTALL_COUNT'] = installments.groupby('SK_ID_CURR').size()
+
+    # clean
+    del df_installments, installments
+    gc.collect()
+
+    return installments_agg
+
+
+def get_credit_card_balance_features(dev_mode=False):
+    num_rows = DEV_SAMPLE_SIZE if dev_mode else None
+
+    df_cc_balance = pd.read_csv(CREDIT_CARD_BALANCE_FILEPATH, nrows=num_rows)
+    cc_balance, cat_cols = one_hot_encoder(df_cc_balance, nan_as_category= True)
+
+    # General aggregations
+    cc_balance = cc_balance.drop(['SK_ID_PREV'], axis= 1)
+    cc_balance_agg = cc_balance.groupby('SK_ID_CURR').agg(['min', 'max', 'mean', 'sum', 'var'])
+    cc_balance_agg.columns = pd.Index(['CC_' + e[0] + "_" + e[1].upper() for e in cc_balance_agg.columns.tolist()])
+
+    # Count credit card lines
+    cc_balance_agg['CC_COUNT'] = cc_balance.groupby('SK_ID_CURR').size()
+
+    # clean
+    del df_cc_balance, cc_balance
+    gc.collect()
+
+    return cc_balance_agg
--- pipelines.py
+++ pipelines.py
@@ -7,7 +7,7 @@ from sklearn import set_config
 from imblearn.pipeline import Pipeline as Pipeline_imb
 from imblearn.over_sampling import SMOTENC, SMOTE
 import transformers
-from data_preprocessing import get_bureau_and_balance_features, get_previous_applications_features
+import data_preprocessing
 
 set_config(transform_output="pandas")
 
@@ -19,7 +19,10 @@ DEFAULT_CONFIG = {
         'should_scale': False,
         'scaler': StandardScaler(),
         'use_bureau_and_balance': False,
-        'use_previous_applications': False
+        'use_previous_applications': False,
+        'use_pos_cash_balance': False,
+        'use_installments_payments': False,
+        'use_credit_card_balance': False,
     },
     'balancing': {
         'should_oversample': False,
@@ -35,15 +38,32 @@ def get_preprocessing_steps(preprocessing_config, balancing_config, dev_mode):
         ('feature_extractor', transformers.ApplicationFeaturesExtractor()),
     ]
 
+    # add bureau balance features
     if preprocessing_config['use_bureau_and_balance']:
-        X_bureau_features = get_bureau_and_balance_features(dev_mode)
+        X_bureau_features = data_preprocessing.get_bureau_and_balance_features(dev_mode)
         steps.append(('merge_bureau_and_balance', transformers.ApplicationFeaturesMerger(X_bureau_features)))
 
+    # add previous applications features
     if preprocessing_config['use_previous_applications']:
-        X_prev_app_features = get_previous_applications_features(dev_mode)
-        print('prev app shape', X_prev_app_features.shape)
+        X_prev_app_features = data_preprocessing.get_previous_applications_features(dev_mode)
         steps.append(('merge_previous_applications', transformers.ApplicationFeaturesMerger(X_prev_app_features)))
 
+    # add pos cash balance features
+    if preprocessing_config['use_pos_cash_balance']:
+        X_pos_cash_balance_features = data_preprocessing.get_pos_cash_balance_features(dev_mode)
+        steps.append(('merge_pos_cash_balance', transformers.ApplicationFeaturesMerger(X_pos_cash_balance_features)))
+
+    # add installments payments features
+    if preprocessing_config['use_installments_payments']:
+        X_installment_features = data_preprocessing.get_installments_payments_features(dev_mode)
+        steps.append(('merge_installments_payments', transformers.ApplicationFeaturesMerger(X_installment_features)))
+
+    # add credit card balance features
+    if preprocessing_config['use_credit_card_balance']:
+        X_cc_balance_features = data_preprocessing.get_credit_card_balance_features(dev_mode)
+        steps.append(('merge_credit_card_balance', transformers.ApplicationFeaturesMerger(X_cc_balance_features)))
+
+    # fill missing values
     if preprocessing_config['should_fill_na']:
         steps.append(('imputer',
                       transformers.ApplicationImputer(num_imputer=preprocessing_config['num_imputer'],
@@ -74,19 +94,34 @@ def dummy_classifier_pipeline(config):
 
 
 def log_reg_pipeline(config, dev_mode=False):
+    verbose = True if dev_mode else False
+
     preprocessing_config = config['preprocessing']
     balancing_config = config['balancing']
 
+    # get preprocessing steps
     steps = get_preprocessing_steps(preprocessing_config, balancing_config, dev_mode)
 
+    # define the classifier
+    classifier = LogisticRegression(
+        random_state=config['model_params']['random_state'],
+        max_iter=2000,
+        **config['model_params']['params']
+    )
+
+    # call imblearn pipeline if oversampling
     if balancing_config['should_oversample']:
-        steps.append(('classifier', LogisticRegression(**config['model_params'])))
-        return Pipeline_imb(steps, verbose=True)
+        steps.append(('classifier', classifier))
+        return Pipeline_imb(steps, verbose=verbose)
 
-    preprocessing_pipeline = Pipeline(steps=steps, verbose=True)
-    steps = [('preprocessor', preprocessing_pipeline), ('classifier', LogisticRegression(**config['model_params']))]
+    # call sklearn pipeline
+    preprocessing_pipeline = Pipeline(steps=steps, verbose=verbose)
+    steps = [
+        ('preprocessor', preprocessing_pipeline),
+        ('classifier', classifier)
+    ]
 
-    return Pipeline(steps, verbose=True)
+    return Pipeline(steps, verbose=verbose)
 
 
 PIPELINES = {
